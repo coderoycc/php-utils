2ef23092cc91f406ffad4c417d5319b79afa627d
Roberto Carlos Chambi Calizaya <90710352+coderoycc@users.noreply.github.com>
2023-06-09 18:53:55 -0400

    conexion php con sqlserver usando "sqlsrv"

---
+++ Bases de datos/conex_php_sql_server.php
@@ -0,0 +1,34 @@
+<?php
+
+    function guidv4(){
+        if (function_exists('com_create_guid') === true)
+            return trim(com_create_guid(), '{}');
+    
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    function formato_fechas_server($dato,$format){
+        if(isset($dato)){
+            $valor = date_format($dato,$format);
+        }else{
+            $valor = '';
+        }
+        return $valor;
+    }
+
+        // session_start();
+        $serverName = "HOSTNAME";
+        //$serverName = "localhost";
+        //Server=localhost;Database=master;Trusted_Connection=True;
+        //$connectionInfo = array("Database"=>"dbBoe"); 
+        $connectionInfo = array("Database"=>"DBPRUEBA", "Uid"=>"USUARIO", "PWD"=>"CONTRASENA", "CharacterSet"=>"UTF-8"); 
+        $con=sqlsrv_connect($serverName,$connectionInfo);
+        if ($con){
+        }else{
+            die( print_r( sqlsrv_errors(), true));
+        }
+?>
+        
