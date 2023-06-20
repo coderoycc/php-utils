727a9d042b9932849681fb04e575b05db3d7b952
Roberto Carlos Chambi Calizaya <90710352+coderoycc@users.noreply.github.com>
2023-06-20 19:11:46 -0400

    Funcion para enviar un FORM de JS a PHP

--- peticiones/js/enviar_form_php.js
+++ peticiones/js/enviar_form_php.js
@@ -1,3 +1,9 @@
+/*
+Esta funcion envia la peticion Mediante POST al archivo shop-details.php
+Y en ese archivo se reciben los valores con 'name'
+es decir, en shop-details se encuentran los valores "id", "idLinea"
+para procesarlos ($_POST['id'])
+*/
 function ver_detalle(id, idLinea) {
   // console.log("id producto: ", id);
   var form = document.createElement("form");
